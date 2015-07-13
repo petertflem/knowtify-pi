18ebb3e135d0d9b03aa1ac58945b790b237f6e5a
petfle <peter.tollnes.flem@knowit.no>
2015-07-13 13:03:12 +0200

    Initial commit. Contains a basic connection to the web socket server.

---
+++ index.js
@@ -0,0 +1,8 @@
+var WebSocket = require('ws');
+
+var host = 'ws://routing-hub.herokuapp.com';
+//var host = 'ws://localhost:5000';
+
+var ws = new WebSocket(host);
+
+//ws.on('open', function open() {});
