ed72863ebf1372be6c07049de147f13f77a86dd0
petfle <peter.tollnes.flem@knowit.no>
2015-07-14 16:47:36 +0200

    Now log data when we receive it.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var WebSocket = require('ws');
 
-var host = 'ws://routing-hub.herokuapp.com';
-//var host = 'ws://localhost:5000';
+//var host = 'ws://routing-hub.herokuapp.com';
+var host = 'ws://localhost:5000';
 var pingId;
 var ws = new WebSocket(host);
 
 ws.on('message', function open(data, flags) {
-  //console.log(data);
+  console.log(data);
 });
 
 ws.on('open', function () {
