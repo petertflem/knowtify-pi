17f160d61fa8e54b9bd1a01ba7399f3d9360df35
Peter Tollnes Flem <peter.t.flem@gmail.com>
2015-07-15 07:36:52 +0200

    Set up ping/pong.

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var WebSocket = require('ws');
 
-//var host = 'ws://routing-hub.herokuapp.com';
-var host = 'ws://localhost:5000';
-
+var host = 'ws://routing-hub.herokuapp.com';
+//var host = 'ws://localhost:5000';
+var pingId;
 var ws = new WebSocket(host);
 
 ws.on('message', function open(data, flags) {
-  console.log(data);
+  //console.log(data);
+});
+
+ws.on('open', function () {
+  pingId = setInterval(function () { ws.ping(); }, 45000);
+});
+
+ws.on('close', function () {
+  clearInterval(pingId);
+  console.log('connection closed by the server');
+});
+
+ws.on('pong', function () {
+  console.log('pong');
 });
