51f7ad846bc57434b8709e0d3fd44f2ea767648b
Peter Tollnes Flem <peter.t.flem@gmail.com>
2015-07-18 01:19:48 +0200

    Finished makinging basic serial communication. I now know sort of how it works.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var WebSocket = require('ws');
-var SerialPort = require('serialport').SerialPort;
 
 /*
  * If no server running, try new connection every x seconds?
  */
 
-var host = 'ws://routing-hub.herokuapp.com';
+/*var host = 'ws://routing-hub.herokuapp.com';
 //var host = 'ws://localhost:5000';
 var pingId;
 var ws = new WebSocket(host);
@@ -22,18 +21,34 @@ ws.on('close', function () {
   clearInterval(pingId);
   console.log('connection closed by the server');
 });
+*/
 
-var serialport = new SerialPort("/dev/ttyACM0", {
-  baudrate: 9600
+var serialport = require('serialport');
+var SerialPort = serialport.SerialPort;
+
+var sp = new SerialPort("/dev/ttyACM0", {
+  baudrate: 9600/*,
+  parser: serialport.parsers.readline('\r\n')*/
 });
 
-serialport.on("open", function () {
-  console.log('open');
-  serialport.on('data', function(data) {
-    console.log('data received: ' + data);
-  });
-  serialport.write("Hello", function(err, results) {
-    console.log('err ' + err);
-    console.log('results ' + results);
-  });
-});
+sp.on('open', showPortOpen);
+sp.on('data', saveLatestData);
+sp.on('close', showPortClose);
+sp.on('error', showError);
+
+function showPortOpen() {
+  console.log('Port open. Data rate: ' + sp.options.baudRate);
+  setInterval(function () { sp.write('Hey there!'); }, 5000);
+}
+
+function saveLatestData(data) {
+  console.log(data);
+}
+
+function showPortClose() {
+  console.log('Port closed.');
+}
+
+function showError(error) {
+  console.log('Serial port error: ' + error);
+}
