c9196151c6d2fd8a1de494b85a8d53d072d0fa80
Peter Tollnes Flem <peter.t.flem@gmail.com>
2015-07-18 23:47:27 +0200

    Working on the serial communication.

--- index.js
+++ index.js
@@ -24,5 +24,16 @@ ws.on('close', function () {
 });
 
 var serialport = new SerialPort("/dev/ttyACM0", {
-  baudrate: 9600 
-	});
+  baudrate: 9600
+});
+
+serialPort.on("open", function () {
+  console.log('open');
+  serialPort.on('data', function(data) {
+    console.log('data received: ' + data);
+  });
+  serialPort.write("Hello", function(err, results) {
+    console.log('err ' + err);
+    console.log('results ' + results);
+  });
+});
