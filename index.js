93c0020543823a65232a9c28d60312b2dc76d420
Peter Tollnes Flem <peter.t.flem@gmail.com>
2015-07-19 14:05:58 +0200

    Now receiving data and passing it on to the arduino.

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 var WebSocket = require('ws');
-
-/*
- * If no server running, try new connection every x seconds?
- */
-
-/*var host = 'ws://routing-hub.herokuapp.com';
+var host = 'ws://routing-hub.herokuapp.com';
 //var host = 'ws://localhost:5000';
 var pingId;
 var ws = new WebSocket(host);
-
-ws.on('message', function open(data, flags) {
-  console.log(data);
-});
-
-ws.on('open', function () {
-  pingId = setInterval(function () { ws.ping(); }, 45000);
-});
-
-ws.on('close', function () {
-  clearInterval(pingId);
-  console.log('connection closed by the server');
-});
-*/
-
 var serialport = require('serialport');
 var SerialPort = serialport.SerialPort;
 
+//If no server running, try new connection every x seconds?
+
+initializeWebSocket();
+
+/*
+{
+  "error-code": 1
+}
+*/
+
+function initializeWebSocket() {
+  ws.on('message', function open(data, flags) {
+    console.log('Data recieved from Heroku: ' + data);
+    writeToSerialPort(data['error-code']);
+  });
+
+  ws.on('open', function () {
+    pingId = setInterval(function () { ws.ping(); }, 45000);
+  });
+
+  ws.on('close', function () {
+    clearInterval(pingId);
+    console.log('connection closed by the server');
+  });
+}
+
 var sp = new SerialPort("/dev/ttyACM0", {
-  baudrate: 9600/*,
-  parser: serialport.parsers.readline('\r\n')*/
+  baudrate: 9600,
+  parser: serialport.parsers.readline('\r\n')
 });
 
 sp.on('open', showPortOpen);
@@ -36,9 +42,13 @@ sp.on('data', saveLatestData);
 sp.on('close', showPortClose);
 sp.on('error', showError);
 
+
+function writeToSerialPort(data) {
+  sp.write(data);
+}
+
 function showPortOpen() {
   console.log('Port open. Data rate: ' + sp.options.baudRate);
-  setInterval(function () { sp.write('Hey there!'); }, 5000);
 }
 
 function saveLatestData(data) {
